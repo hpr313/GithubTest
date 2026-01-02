f9c5259d5f28047156c34a05cf664ee526e41590
hpr31 <hpr313@gmail.com>
2026-01-02 16:10:00 +0900

    Test

--- GithubTest/Form1.vb
+++ GithubTest/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show("Hello Perry! Happy New Year!")
+        MessageBox.Show("Hello Saito San! Happy New Year!")
     End Sub
 End Class
 
